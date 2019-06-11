e8efe011ef8e58c204f7044b9a932ef0202333ed
PaveTranquil <pasha.2ovchinnikov@gmail.com>
2019-06-12 01:20:11 +0300

    Cosmetic fix
    - smiles
    - words
    - if conditional by PEP

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ for event in longpoll.listen():
 		id = event.object.peer_id
 		text = event.object.text
 		
-		if payload == 'wait idea' and text != 'Вернуться ↩': payload = 'sending idea'
-		else: payload = event.object.payload
+		if payload == 'wait idea' and text != 'Вернуться ↩':
+			payload = 'sending idea'
+		else:
+			payload = event.object.payload
 
 		if payload == '{"command":"start"}': 
-			msg(id, 'Привет, я PTBot, дворецкий команды PTCodding. \nНажмите на нужную Вам кнопку, чтобы команда нашла Вас и быстро ответила, а я не потерял Вас =) \n\n#idea — идеи и предложения \n#partnership — партнёрство, сотрудничество, спонсорство \n#support — администрация, помощь и вопросы \n#buy — товары, магазин и покупки', board=keyboards.menu)
+			msg(id, 'Привет, я PTBot, дворецкий команды PTCodding. \nНажмите на нужную Вам кнопку, чтобы команда нашла Вас и быстро ответила, а я не потерял Вас =) \n\n#idea — идеи и предложения \n#partnership — партнёрство, сотрудничество, спонсорство \n#support — администрация, помощь и вопросы \n#buy — магазин услуг и покупки', board=keyboards.menu)
 
 		print('{} отправляет сообщение с текстом "{}"'.format(id, text))
 
@@ -25,15 +27,18 @@ for event in longpoll.listen():
 
 			elif payload == 'sending idea':
 				attachs	= give_attachs(event.object.id)
-				if attachs[0] != '': print('{} отправляет идею и прикрепляет {}'.format(id, attachs[0]))
-				else: print('{} отправляет идею'.format(id))
+				if attachs[0] != '':
+					print('{} отправляет идею и прикрепляет {}'.format(id, attachs[0]))
+				else:
+					print('{} отправляет идею'.format(id))
 
-				if attachs[1] == 0: msg(2000000002, '#botidea \n[id{}|{} {}] предлагает идею: \n{}\n\nОтветить пользователю: https://vk.com/gim132868814?sel={}'.format(id ,name(id)[0]['first_name'], name(id)[0]['last_name'], text, id), attach=attachs[0])
+				if attachs[1] == 0:
+					msg(2000000002, '#botidea \n[id{}|{} {}] предлагает идею: \n{}\n\nОтветить пользователю: https://vk.com/gim132868814?sel={}'.format(id ,name(id)[0]['first_name'], name(id)[0]['last_name'], text, id), attach=attachs[0])
 				else:
 					msg(2000000002, '#botidea \n[id{}|{} {}] предлагает идею, отправляя стикер. \nОтветить пользователю: https://vk.com/gim132868814?sel={}'.format(id ,name(id)[0]['first_name'], name(id)[0]['last_name'], id), attach=attachs[0])
 					msg(2000000002, stick=attachs[1])
 
-				msg(id, 'Ваша идея будет доставлена команде PTCodding в аккуратном конвертике с Вашей печатью. Ожидайте ответа!', board=keyboards.menu)
+				msg(id, 'Ваша идея будет доставлена команде PTCodding в аккуратном конвертике с Вашей печатью. Ожидайте ответа! &#8986;', board=keyboards.menu)
 
 			elif payload == '{"command":"partnership"}':
 				msg(id, 'Мы работаем над этим разделом...\nSoon...', board=keyboards.back)
@@ -63,7 +68,7 @@ for event in longpoll.listen():
 				msg(id, 'У вас есть возможность отправить Ваше местоположение сразу для заказа звукозаписи или ремонта ПК.', board=keyboards.locateboard())
 
 			elif payload == '{"command":"sent_location"}':
-				msg(id, 'Спасибо за отправку Вашего местоположения! Команда PTCodding обязательно учтёт это при оформлении заказа!', board=keyboards.buy)
+				msg(id, 'Спасибо за отправку Вашего местоположения! Команда PTCodding обязательно учтёт это при оформлении заказа! &#128521;', board=keyboards.buy)
 					
 			elif payload == '{"command":"basket"}':
 				msg(id, 'Ваша корзина расположена по этой ссылке: https://vk.com/ptcodding?w=app6468267_-132868814', board=keyboards.buyback)
@@ -72,17 +77,16 @@ for event in longpoll.listen():
 				msg(id, 'Возвращаю Вас в меню товаров. Напоминаю назначение кнопок: \n\n', board=keyboards.buy)
 
 			elif payload == '{"command":"donat"}':
-				msg(id, 'Я очень хочу кушать. Я голодный... Дайте, пожалуйста, пару долларов, чтобы мне купили пончик.', board=keyboards.payboard('action=transfer-to-group&group_id=132868814&aid=10'))
+				msg(id, 'Я очень хочу кушать. Я голодный... &#128546; Дайте, пожалуйста, пару долларов, чтобы мне купили пончик. &#127849;', board=keyboards.payboard('action=transfer-to-group&group_id=132868814&aid=10'))
 
 			elif payload == '{"command":"partners"}':
 				msg(id, 'Добро пожаловать в список партнёров команды PTCodding! Я от лица команды говорю жизни спасибо за то, что она свела нас с этими людьми, ведь без их поддержки и помощи у нас бы ничего не получилось бы! &#128079;', board=keyboards.partnerboard(id))
 			
 			elif payload == '{"command":"sapod"}':
-				msg(id, '@sapod (SAPOD) — первый и единственный подкаст из мира San Andreas. \n\nВедущий подкаста Стич часто появляется и в подкастах от PTCodding. Вместе с Павлом они обсуждают новости уходящего месяца в IT-кухне и жарко спорят, кто лучше: iOS или Android &#128521;\n\nСлушайте Стича в его подкасте SAPOD — vk.com/sapod')
+				msg(id, '@sapod (SAPOD) — первый и единственный подкаст из мира San Andreas. \n\nВедущий подкаста Стич часто появляется и в подкастах от PTCodding. Вместе с Павлом они обсуждают новости уходящего месяца в IT-кухне и жарко спорят, кто лучше: iOS или Android &#128521;\n\nСлушайте Стича в его подкасте SAPOD — vk.com/sapod &#128072;')
 
 			elif payload == '{"command":"back"}':
-				msg(id, 'Возвращаю Вас в главное меню. Напоминаю назначение кнопок: \n\n#idea — идеи и предложения \n#partnership — партнёрство, сотрудничество, спонсорство \n#support — администрация, помощь и вопросы \n#buy — товары, магазин и покупки', board=keyboards.menu)
-				payload = ''
+				msg(id, 'Возвращаю Вас в главное меню. Напоминаю назначение кнопок: \n\n#idea — идеи и предложения \n#partnership — партнёрство, сотрудничество, спонсорство \n#support — администрация, помощь и вопросы \n#buy — магазин услуг и покупки', board=keyboards.menu)
 
 			elif event.object.text.find('/restartptbot0921') != -1:
 				msg(id, 'Перезапуск клавиатуры...', board=keyboards.menu)
