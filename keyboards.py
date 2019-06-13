083063867d820e9e344b45cdd8d241f17a591227
PaveTranquil <pasha.2ovchinnikov@gmail.com>
2019-06-13 20:15:05 +0300

    Pre-release #buy
    - add carts and cartboard
    - 4 buttons as items in shop
    - delete sending location
    - add VK shop
    - add itemboard for items
    - printing carts to console
    - paying and ordering by replying

--- keyboards.py
+++ keyboards.py
@@ -3,6 +3,8 @@ from vk_api.keyboard import VkKeyboard
 
 back, menu, buy, buyback, partner = VkKeyboard(True), VkKeyboard(True), VkKeyboard(True), VkKeyboard(True), VkKeyboard(False)
 
+carts = dict()
+
 def backboard(back):
 	back.add_button('Вернуться &#8617;', 'negative', '{"command":"back"}')
 	return back.get_keyboard()
@@ -22,19 +24,45 @@ def menuboard(menu):
 	return menu.get_keyboard()
 
 def buyboard(buy):
-	buy.add_button('Реклама &#128640;', 'primary', '{"command":"pr"}')
-	buy.add_button('Code от PaveTranquil &#128104;', 'primary', '{"command":"code"}')
-	buy.add_button('Design от BogToGus &#128142;', 'primary', '{"command":"design"}')
+	buy.add_button('Code от Павла', 'primary', '{"command":"code"}')
+	buy.add_button('Design от Богдана', 'primary', '{"command":"design"}')
 	buy.add_line()
-	buy.add_button('Звукозапись &#127911;', 'primary', '{"command":"record"}')
-	buy.add_button('Ремонт ПК &#128187;', 'primary', '{"command":"fix"}')
+	buy.add_button('Звукозапись', 'primary', '{"command":"record"}')
+	buy.add_button('Ремонт ПК ', 'primary', '{"command":"fix"}')
 	buy.add_line()
-	buy.add_button('Добавить к заказу своё местоположение &#127757;', payload='{"command":"location"}')
+	buy.add_button('Корзина &#128722;', payload='{"command":"cart"}')
 	buy.add_line()
-	buy.add_button('Перейти в корзину &#128722;', payload='{"command":"basket"}')
+	buy.add_button('Наш магазин ВКонтакте &#128041;', payload='{"command":"cart_ui"}')
 	buy.add_line()
 	buy.add_button('Вернуться &#8617;', 'negative', '{"command":"back"}')
 	return buy.get_keyboard()
+
+def cartboard(id, item='', adding=True):
+	if id not in carts:
+		carts[id] = []
+
+	board = VkKeyboard(True)
+	cart = carts[id]
+
+	if item:
+		if item not in cart and adding:
+			cart.append(item)
+		if item in cart and not adding:
+			cart.remove(item)
+
+	for it in cart:
+		board.add_button(it.split('-')[0]+' &#10134;', payload='{"command":"delete_{}"}'.format(it.split('-')[1]))
+		board.add_line()
+	if cart:
+		board.add_button('Оформить заказ &#128222;', 'positive', '{"command":"book"}')
+		board.add_line()
+	if item:
+		board.add_button('Вернуться &#8617;', 'negative', '{"command":"{}"}'.format(item.split('-')[1]))
+	else
+		board.add_button('Вернуться &#8617;', 'negative', '{"command":"back_buy"}')
+
+	return board.get_keyboard()
+
 
 def buybackboard(buyback):
 	buyback.add_button('Вернуться &#8617;', 'negative', '{"command":"back_buy"}')
@@ -54,6 +82,9 @@ def appboard(id):
 
 def locateboard():
 	return json.dumps({"one_time":True, "buttons":[[{"action":{"type":"location", "payload":"{\"command\":\"sent_location\"}"}}], [{"color":"negative","action":{"type":"text","payload":"{\"command\":\"back_buy\"}","label":"Вернуться &#8617;"}}]]}, ensure_ascii=False)
+
+def itemboard(item_name):
+	return json.dumps({"one_time":True, "buttons":[[{"color":"primary", "action":{"type":"text", "payload":"{\"command\":\"add_{}\"}".format(item_name), "label":"Добавить в корзину &#10133;"}}], [{"color":"negative","action":{"type":"text","payload":"{\"command\":\"back_buy\"}","label":"Вернуться &#8617;"}}]]}, ensure_ascii=False)
 
 def payboard(hash):
 	return json.dumps({"one_time":True, "buttons":[[{"action":{"type":"vkpay", "hash":hash}}], [{"color":"negative","action":{"type":"text","payload":"{\"command\":\"back\"}","label":"Вернуться &#8617;"}}]]}, ensure_ascii=False)
--- main.py
+++ main.py
@@ -33,7 +33,7 @@ try:
 					else:
 						print('{} отправляет идею'.format(id))
 
-				msg(2000000002, '#botidea \n[id{}|{} {}] предлагает идею: \n{}\n\nОтветить пользователю: https://vk.com/gim132868814?sel={}'.format(id ,name(id)['first_name'], name(id)['last_name'], text, id), attach=attachs)
+					msg(2000000002, '#botidea \n[id{}|{} {}] предлагает идею: \n{}\n\nОтветить пользователю: https://vk.com/gim132868814?sel={}'.format(id ,name(id)['first_name'], name(id)['last_name'], text, id), attach=attachs)
 
 					msg(id, 'Ваша идея будет доставлена команде PTCodding в аккуратном конвертике с Вашей печатью. Ожидайте ответа! &#8986;', keyboards.menu)
 
@@ -43,53 +43,54 @@ try:
 				elif payload == '{"command":"support"}':
 					msg(id, 'Мы работаем над этим разделом...\nSoon...', keyboards.back)
 
-			elif payload == '{"command":"buy"}':
-				msg(id, 'Мы работаем над этим разделом...\nSoon...', board=keyboards.buy)
+				elif payload == '{"command":"buy"}':
+					msg(id, 'В разделе покупок Вы можете приобрести у нас виртуальные услуги и услуги на дом — рекламу, программирование и дизайн от моего начальства, звукозапись и ремонт Вашего ПК или Android-смартфона. &#128230; \nЕщё если Вы вдруг потеряетесь, то всегда можете заглянуть в привычную корзину и магазин от ВКонтакте))) &#128071;', keyboards.buy)
 
-			elif payload == '{"command":"pr"}':
-				msg(id, 'Мы работаем над этим разделом...\nSoon...', board=keyboards.buyback)
+				elif payload == '{"command":"code"}':
+					msg(id, 'Цена: от 200 руб. &#128181; \n\nМой начальник Павел вместе с командой создадут любую программу под Ваши цели и по Вашим требованиям. Ваши данные: описание функционала и ключевых целей и задач программы. \nХочу предупредить Вас: если Вам не понравилась программа, есть какие-то проблемы, ошибки, баги, то необходимо сразу же сообщить об этом. Любые недочёты исправляются одним нажатием клавиши. \nВ программе прописаны копирайты, с указанием авторства на сообщество PTCodding. Просьбы вернуть деньги после того, как Вы оплатили заказ, не принимаются. \nЦена может изменяться в зависимости от срока заказа и формата работы, указана минимальная цена! Также могут появляться специальные условия! \n\nСсылка на товар в нашем магазине ВКонтакте: vk.com/ptcodding?w=product-132868814_2055522 &#128072;', keyboards.itemboard('code'))
 
-			elif payload == '{"command":"code"}':
-				msg(id, 'Мы работаем над этим разделом...\nSoon...', keyboards.buyback)
+				elif payload == '{"command":"add_code"}':
+					msg(id, 'Товар добавлен в Вашу корзину', keyboards.cartboard(id, 'Code от Павла-code'))
 
-			elif payload == '{"command":"design"}':
-				msg(id, 'Мы работаем над этим разделом...\nSoon...', keyboards.buyback)
+				elif payload == '{"command":"delete_code"}':
+					msg(id, 'Товар удалён из Вашей корзины', keyboards.cartboard(id, 'Code от Павла-code', False))
 
-			elif payload == '{"command":"record"}':
-				msg(id, 'Мы работаем над этим разделом...\nSoon...', keyboards.buyback)
+				elif payload == '{"command":"design"}':
+					msg(id, 'Цена: от 200 руб. &#128181; \n\nДизайнер команды PTCodding создаст дизайн Вашему бренду по Вашим требованиям. Ваши данные: название бренда, для которой делается аватарка, дизайн \nХочу предупредить Вас: если Вам не понравился какой-то элемент дизайна, то ему необходимо сразу написать об этом. Любые недочёты исправляются одним нажатием клавиши. \nНа весь пак-дизайн, до оплаты заказа, будет нанесён водяной знак. Просьбы вернуть деньги после того, как Вы оплатили заказ и пак был передан без водяных знаков, не принимаются. \nЦена может изменяться в зависимости от срока заказа и техники выполнения, указана минимальная цена! Также могут появляться специальные условия! \n\nСсылка на товар в нашем магазине ВКонтакте: vk.com/ptcodding?w=product-132868814_2064351 &#128072;', keyboards.itemboard('design'))
 
-			elif payload == '{"command":"fix"}':
-				msg(id, 'Мы работаем над этим разделом...\nSoon...', keyboards.buyback)
+				elif payload == '{"command":"add_design"}':
+					msg(id, 'Товар добавлен в Вашу корзину', keyboards.cartboard(id, 'Design от Богдана-design'))
 
-			elif payload == '{"command":"location"}':
-				msg(id, 'У Вас есть возможность отправить Ваше местоположение сразу для заказа звукозаписи или ремонта ПК.', keyboards.locateboard())
+				elif payload == '{"command":"delete_design"}':
+					msg(id, 'Товар удалён из Вашей корзины', keyboards.cartboard(id, 'Design от Богдана-design', False))
 
-			elif payload == '{"command":"sent_location"}':
-				msg(id, 'Спасибо за отправку Вашего местоположения! Команда PTCodding обязательно учтёт это при оформлении заказа! &#128521;', keyboards.buy)
-					
-			elif payload == '{"command":"basket"}':
-				msg(id, 'Ваша корзина расположена по этой ссылке: https://vk.com/ptcodding?w=app6468267_-132868814', board=keyboards.buyback)
-			
-			elif payload == '{"command":"back_buy"}':
-				msg(id, 'Возвращаю Вас в меню товаров.', keyboards.buy)
+				elif payload == '{"command":"record"}':
+					msg(id, 'Мы работаем над этим разделом...\nSoon...', keyboards.itemboard('record'))
 
-			elif payload == '{"command":"news"}':
-				msg(id, 'Мы работаем над этим разделом...\nSoon...', keyboards.back)
+				elif payload == '{"command":"add_record"}':
+					msg(id, 'Товар добавлен в Вашу корзину', keyboards.cartboard(id, 'Звукозапись-record'))
 
-			elif payload == '{"command":"donat"}':
-				msg(id, 'Я очень хочу кушать. Я голодный... &#128546; Дайте, пожалуйста, пару долларов, чтобы мне купили пончик. &#127849;', keyboards.payboard('action=transfer-to-group&group_id=132868814&aid=10'))
+				elif payload == '{"command":"delete_record"}':
+					msg(id, 'Товар удалён из Вашей корзины', keyboards.cartboard(id, 'Звукозапись-record', False))
 
-			elif payload == '{"command":"partners"}':
-				msg(id, 'Добро пожаловать в список партнёров команды PTCodding! Я от лица команды говорю жизни спасибо за то, что она свела нас с этими людьми, ведь без их поддержки и помощи у нас бы ничего не получилось бы! &#128079;', keyboards.partner)
-			
-			elif payload == '{"command":"sapod"}':
-				msg(id, '@sapod (SAPOD) — первый и единственный подкаст из мира San Andreas. \n\nВедущий подкаста Стич часто появляется и в подкастах от PTCodding. Вместе с Павлом они обсуждают новости уходящего месяца в IT-кухне и жарко спорят, кто лучше: iOS или Android &#128521;\n\nСлушайте Стича в его подкасте SAPOD — vk.com/sapod &#128072;')
+				elif payload == '{"command":"fix"}':
+					msg(id, 'Мы работаем над этим разделом...\nSoon...', keyboards.itemboard('fix'))
 
-			elif payload == '{"command":"tryout"}':
-				msg(id, 'TryOut — платформа для тестирования сервисов VK Mini Apps. \n\nНаш недавний партнёр @hit2hat (Степан Новожилов) делает так, чтобы обнаружение и исправление багов не было рутиной как для тестеров, так и для самих разработчиков. \nПавел и Степан давно работают вместе над разными проектами и постигают хакатоны. &#9994; \n\nЗаглядывайте в TryOut Степана — там PTCodding иногда проводит бета-тестирования своих проектов &#128071;', keyboards.appboard(id))
-			
-			elif payload == '{"command":"back"}':
-				msg(id, 'Возвращаю Вас в главное меню. Напоминаю назначение кнопок: \n\n#idea — идеи и предложения \n#partnership — партнёрство, сотрудничество, спонсорство \n#support — администрация, помощь и вопросы \n#buy — магазин услуг и покупки', keyboards.menu)
+				elif payload == '{"command":"add_fix"}':
+					msg(id, 'Товар добавлен в Вашу корзину', keyboards.cartboard(id, 'Ремонт ПК-fix'))
+
+				elif payload == '{"command":"delete_fix"}':
+					msg(id, 'Товар удалён из Вашей корзины', keyboards.cartboard(id, 'Ремонт ПК-fix', False))
+
+				elif payload == '{"command":"cart"}':
+					msg(id, 'Корзина товаров, которые Вы добавили к заказу. \nТовары удаляются из корзины при нажатии на них. Если Вы добавили в корзину какие-то товары, то будет доступна кнопка оформления заказа — нажмите на неё, и с Вами в этом чате свяжутся мои операторы, которые обсудят все условия и цены &#128222;', keyboards.cartboard(id))
+						
+				elif payload == '{"command":"cart_ui"}':
+					msg(id, 'Загляните в привычный магазин, если мой Вам оказался не по душе: https://vk.com/market-132868814 &#128072;', keyboards.buyback)
+				
+				elif payload == '{"command":"back_buy"}':
+					msg(id, 'Возвращаю Вас в меню товаров.', keyboards.buy)
+
 				elif payload == '{"command":"news"}':
 					msg(id, 'Мы работаем над этим разделом...\nSoon...', keyboards.back)
 
@@ -114,12 +115,21 @@ try:
 			text = event.object.text
 			id = event.object.peer_id
 
+			if text.find('Заказ оформлен') != -1:
+				msg(text.split()[-1], 'Спасибо Вам за оформление заказа. В дальнейшем мои операторы будут поддерживать с Вами периодическую связь по поводу заказа, а я всегда работаю здесь для Вас, в этом чате ', keyboards.menu)
+				payload = ''
+
 			elif text.find('Перезапуск') != -1:
 				msg(text.split()[-1], 'Меня перезапустили. Не знаю, почему, но так надо, видимо.')
 
 			elif text.find('Баг-перезапуск') != -1:
 				msg(text.split()[-1], 'Добрый день! Видимо, наш PTBot где-то сломался, но сейчас уже всё хорошо. Приносим свои извинения, и перезапускаем его...')
 				msg(text.split()[-1], 'Привет, это снова я, Ваш любимый PTBot! &#128075; Добро пожаловать в старое доброе меню — где какие кнопки, я думаю, Вы и сами знаете! &#128526;')
+
+			elif text.find('Оплатить-') != -1:
+				amount = text.split('-')[1]
+				description = text.split()[3].split('"')[1]
+				msg(id, 'Меня попросили запустить для Вас оплату «{}» на сумму в ₽{}. Подтвердите оплату...'.format(description, amount), keyboards.payboard('action=pay-to-group&amount={}&description={}&group_id=132868814&aid=10'.format(amount, description)))
 
 
 
@@ -132,4 +142,5 @@ try:
 				msg(2000000002, '{} пожертвовал ₽{}'.format(name(id), amount))
 
 except Exception:
+	print(keyboards.carts)
 	msg(2000000002, 'Бот упал с лестницы самодержавия! @pavetranquil (Павел), помоги ему подняться — исправь баг &#128513;')
