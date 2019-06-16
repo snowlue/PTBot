6357e54213ca6ef9f6508c14dc205d30eb4e53b5
PaveTranquil <pasha.2ovchinnikov@gmail.com>
2019-06-16 16:12:10 +0300

    Bugfix #1 of last commit

--- methods.py
+++ methods.py
@@ -5,8 +5,8 @@ def msg(id, message='', board=[], forward=''):
 	if board:
 		vk.messages.send(peer_id=id, random_id=random.randint(-2147483648, 2147483647), message=message, forward_messages=forward, keyboard=board)
 	else:
-		vk_session.method('messages.send', {'peer_id': id, 'random_id': random.randint(-2147483648, 2147483647), 'message': message, 'attachment': attach, 'keyboard': board})
-	print('Сообщение с текстом «{}» для {} отправлено'.format(message ,id))
+		vk_session.method('messages.send', {'peer_id': id, 'random_id': random.randint(-2147483648, 2147483647), 'message': message, 'forward_messages': forward, 'keyboard': board})
+	print('Сообщение с текстом «{}» для {} отправлено'.format(message, id))
 
 def name(id, case='nom'):
 	return vk.users.get(user_ids=id, fields='first_name, last_name', name_case=case)[0]
