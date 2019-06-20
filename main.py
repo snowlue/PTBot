f6720c816a48545c3f94a7f7beb7e18a80ec00e9
PaveTranquil <pasha.2ovchinnikov@gmail.com>
2019-06-20 08:42:26 +0300

    Bugfix #5 of all waitings

--- main.py
+++ main.py
@@ -14,17 +14,21 @@ for event in longpoll.listen():
 		payload = event.object.payload
 		
 		if id == 2000000002:
-			if state_chat == 'wait request_id' and text.split()[1] != 'Вернуться ↩':
+			if state_chat == 'wait request_id' and ' '.join(text.split()[1:]) != 'Вернуться ↩':
 				state_chat = 'sending request_id'
-			elif state_chat == 'wait amount' and text.split()[1] != 'Вернуться ↩':
+			elif state_chat == 'wait amount' and ' '.join(text.split()[1:]) != 'Вернуться ↩':
 				state_chat = 'sending amount'
-			elif state_chat == 'wait description' and text.split()[1] != 'Вернуться ↩':
+			elif state_chat == 'wait description' and ' '.join(text.split()[1:]) != 'Вернуться ↩':
 				state_chat = 'sending description'
 			elif state_chat == 'wait restart_id' and ' '.join(text.split()[1:]) != 'Вернуться ↩':
 				state_chat = 'sending restart_id'
+			else:
+				state_chat = payload
 		if id < 2000000000:
 			if state == 'wait idea' and text != 'Вернуться ↩':
 				state = 'sending idea'
+			else:
+				state = payload
 
 		if id == 2000000002 and (payload == '{"command":"start"}' or text.lower().find('начать') != -1):
 			msg(id, 'Привет, команда PTCodding! Рад вас видеть! Вижу, что этот чат — чат моих создателей. Включаю дополнительные функции &#128522; \n\n#news — последние новости из сферы IT \nЗапрос VK Pay — запрос средств с указанием amount, description и id \nБаг-перезапуск — перезапуск бота по id с сообщением о баге', keyboards.chat)
@@ -34,7 +38,7 @@ for event in longpoll.listen():
 		print('{} отправляет сообщение с текстом "{}"'.format(id, text))
 
 		if id in [223166352, 165504240, 186823615, 484735371, 2000000002]:
-			if payload == '{"command":"idea"}':
+			if state == '{"command":"idea"}':
 				msg(id, "Предложите свою идею для PTCodding! Я pассмотрю её, и команда PTCodding обязательно отпишется Вам в этом диалоге. \nПостарайтесь соблюдать структуру: \n1. Лаконичное название, отражающее суть идеи \n2. Собственно идея, её развёртка \n3. Средства и блага, необходимые для развёртки Вашей идеи \n4. Расскажите, чем Ваша идея поможет сообществу \nНе забудьте, что необходимо уместить Вашу идею в рамках одного сообщения. Спасибо за Ваше содействие и помощь! \n\nС уважением, PTBot.", keyboards.back)
 				state = 'wait idea'
 
@@ -45,77 +49,77 @@ for event in longpoll.listen():
 
 				msg(id, 'Ваша идея будет доставлена команде PTCodding в аккуратном конвертике с Вашей печатью. Ожидайте ответа! &#8986;', keyboards.menu)
 
-			elif payload == '{"command":"partnership"}':
+				msg(id, 'Мы работаем над этим разделом...\nSoon...', keyboards.back)
+			elif state == '{"command":"partnership"}':
+
+			elif state == '{"command":"support"}':
 				msg(id, 'Мы работаем над этим разделом...\nSoon...', keyboards.back)
 
-			elif payload == '{"command":"support"}':
-				msg(id, 'Мы работаем над этим разделом...\nSoon...', keyboards.back)
-
-			elif payload == '{"command":"buy"}':
+			elif state == '{"command":"buy"}':
 				msg(id, 'В разделе покупок Вы можете приобрести у нас виртуальные услуги и услуги на дом — рекламу, программирование и дизайн от моего начальства, звукозапись и ремонт Вашего ПК или Android-смартфона. &#128230; \nЕщё если Вы вдруг потеряетесь, то всегда можете заглянуть в привычную корзину и магазин от ВКонтакте))) &#128071;', keyboards.buy)
 
-			elif payload == '{"command":"code"}':
+			elif state == '{"command":"code"}':
 				msg(id, 'Цена: от 200 руб. &#128181; \n\nМоё начальство напишет чат-бота ВКонтакте или в Telegram под Ваши цели и по Вашим требованиям. Ваши данные: описание функционала и ключевых целей и задач чат-бота, сгенерированный longpoll-токен. \nХочу предупредить Вас: если Вам не понравился чат-бот, есть какие-то проблемы, ошибки, баги, то необходимо сразу же сообщить об этом. Любые недочёты исправляются одним нажатием клавиши. \nВ чат-боте прописан его создатель, с указанием авторства на сообщество PTCodding. Просьбы вернуть деньги после того, как Вы установили чат-бота в своё сообщество, не принимаются. \nЦена может изменяться в зависимости от срока заказа, указана минимальная цена! Также могут появляться специальные условия! \n\nСсылка на товар в нашем магазине ВКонтакте: vk.com/ptcodding?w=product-132868814_2055522 &#128072;', keyboards.itemboard('code'))
-			elif payload == '{"command":"add_code"}':
+			elif state == '{"command":"add_code"}':
 				msg(id, 'Товар добавлен в Вашу корзину', keyboards.cartboard(id, 'Создание чат-бота—code'))
-			elif payload == '{"command":"delete_code"}':
+			elif state == '{"command":"delete_code"}':
 				msg(id, 'Товар удалён из Вашей корзины', keyboards.cartboard(id, 'Создание чат-бота—code', False))
 
-			elif payload == '{"command":"design"}':
+			elif state == '{"command":"design"}':
 				msg(id, 'Цена: от 200 руб. &#128181; \n\nМоё начальство создаст дизайн Вашему бренду по Вашим требованиям. Ваши данные: название бренда, для которого делается дизайн, стиль исполнения. \nХочу предупредить Вас: если Вам не понравился какой-то элемент дизайна, то ему необходимо сразу написать об этом. Любые недочёты исправляются одним нажатием клавиши. \nНа весь пак дизайна, до оплаты заказа, будет нанесён водяной знак. Просьбы вернуть деньги после того, как Вы оплатили заказ и пак был передан без водяных знаков, не принимаются. \nЦена может изменяться в зависимости от срока заказа и техники выполнения, указана минимальная цена! Также могут появляться специальные условия! \n\nСсылка на товар в нашем магазине ВКонтакте: vk.com/ptcodding?w=product-132868814_2064351 &#128072;', keyboards.itemboard('design'))
-			elif payload == '{"command":"add_design"}':
+			elif state == '{"command":"add_design"}':
 				msg(id, 'Товар добавлен в Вашу корзину', keyboards.cartboard(id, 'Создание дизайна—design'))
-			elif payload == '{"command":"delete_design"}':
+			elif state == '{"command":"delete_design"}':
 				msg(id, 'Товар удалён из Вашей корзины', keyboards.cartboard(id, 'Создание дизайна—design', False))
 
-			elif payload == '{"command":"record"}':
+			elif state == '{"command":"record"}':
 				msg(id, 'Цена: от 750 руб. &#128181; \n\nМоё начальство запишет Ваш голос на своей домашней студии, обработает звук и отправит исходники в mp3, wav или другом формате на флешку, ВКонтакте или в облако. \nПри заказе дополнительно сведения, мастеринга или же при полноценной запись под ключ цена оговаривается при оформлении заказа. \nХочу предупредить Вас: на весь звуковой файл накладывается демо-фраза до оплаты заказа. Просьбы вернуть деньги после того, как Вы оплатили заказ и файл был передан без демо-фразы, не принимаются. \nЦена может изменяться в зависимости от срока заказа и дополнительных услуг, указана минимальная цена! Для клиентов могут появляться специальные условия! \n\nСсылка на товар в нашем магазине ВКонтакте: vk.com/ptcodding?w=product-132868814_2561900 &#128072;', keyboards.itemboard('record'))
-			elif payload == '{"command":"add_record"}':
+			elif state == '{"command":"add_record"}':
 				msg(id, 'Товар добавлен в Вашу корзину', keyboards.cartboard(id, 'Звукозапись—record'))
-			elif payload == '{"command":"delete_record"}':
+			elif state == '{"command":"delete_record"}':
 				msg(id, 'Товар удалён из Вашей корзины', keyboards.cartboard(id, 'Звукозапись—record', False))
 
-			elif payload == '{"command":"fix"}':
+			elif state == '{"command":"fix"}':
 				msg(id, 'Цена: от 500 руб. &#128181; \n\nМоё начальство также работает с компьютерами и смартфонами: исправляет мелкие неполадки в работе, устанавливает лицензированный Microsoft Office или иное ПО, устанавливают активированную Windows, перепрошивают Android-смартфоны. \nХочу предупредить Вас: проверяйте работоспособность ПО, ПК или смартфона в целом сразу перед оплатой заказа. PTCodding даёт бесплатную гарантию — 1 месяц. Далее гарантия продлевается платно. \nЦена может изменяться в зависимости от срока заказа, указана минимальная цена! Для клиентов могут появляться специальные условия! \n\nСсылка на товар в нашем магазине ВКонтакте: vk.com/ptcodding?w=product-132868814_2561901 &#128072;', keyboards.itemboard('fix'))
-			elif payload == '{"command":"add_fix"}':
+			elif state == '{"command":"add_fix"}':
 				msg(id, 'Товар добавлен в Вашу корзину', keyboards.cartboard(id, 'ПК и смартфоны—fix'))
-			elif payload == '{"command":"delete_fix"}':
+			elif state == '{"command":"delete_fix"}':
 				msg(id, 'Товар удалён из Вашей корзины', keyboards.cartboard(id, 'ПК и смартфоны—fix', False))
 
-			elif payload == '{"command":"cart"}':
+			elif state == '{"command":"cart"}':
 				msg(id, 'Корзина товаров, которые Вы добавили к заказу. \nТовары удаляются из корзины при нажатии на них. Если Вы добавили в корзину какие-то товары, то будет доступна кнопка оформления заказа — нажмите на неё, и с Вами в этом чате свяжутся мои операторы, которые обсудят все условия и цены &#128222;', keyboards.cartboard(id))
 					
-			elif payload == '{"command":"cart_ui"}':
+			elif state == '{"command":"cart_ui"}':
 				msg(id, 'Загляните в привычный магазин, если мой Вам оказался не по душе: https://vk.com/market-132868814 &#128072;', keyboards.buyback)
 
-			elif payload == '{"command":"order"}':
+			elif state == '{"command":"order"}':
 				msg(2000000002, 'Вас вызывают для оформления заказа. Пройдите, пожалуйста, по этой ссылке: https://vk.com/gim132868814?sel={}'.format(id))
 				msg(id, 'Ожидайте... Скоро мои операторы свяжутся с Вами для уточнения деталей и оплаты заказа! &#8986;')
 			
-			elif payload == '{"command":"back_buy"}':
+			elif state == '{"command":"back_buy"}':
 				msg(id, 'Возвращаю Вас в меню товаров.', keyboards.buy)
 
-			elif payload == '{"command":"news"}':
+			elif state == '{"command":"news"}':
 				msg(id, 'Мы работаем над этим разделом...\nSoon...', keyboards.back)
 
-			elif payload == '{"command":"donat"}':
+			elif state == '{"command":"donat"}':
 				msg(id, 'Я очень хочу кушать. Я голодный... &#128546; Дайте, пожалуйста, пару долларов, чтобы мне купили пончик. &#127849;', keyboards.donatboard('action=transfer-to-group&group_id=132868814&aid=10'))
 
-			elif payload == '{"command":"app_donat"}':
+			elif state == '{"command":"app_donat"}':
 				msg(id, 'Если хотите поддержать наш проект, то лучше сделайте пожертвование через приложение — так Вы не просто поддержите нас, но и продвинете проект! &#128200;', keyboards.appboard(6887721, -132868814, 'Пончик — наши донаты &#127849;'))
 			
-			elif payload == '{"command":"partners"}':
+			elif state == '{"command":"partners"}':
 				msg(id, 'Добро пожаловать в список партнёров команды PTCodding! Благодаря этим людям в PTCodding очень многое изменяется. Я как чат-бот сообщества наблюдаю за всем этим со стороны — PTCodding растёт на глазах! &#128079;', keyboards.partner)
 			
-			elif payload == '{"command":"sapod"}':
+			elif state == '{"command":"sapod"}':
 				msg(id, '@sapod (SAPOD) — первый и единственный подкаст из мира San Andreas. \n\nВедущий подкаста Стич часто появляется и в подкастах от PTCodding. Вместе с Павлом они обсуждают новости уходящего месяца в IT-кухне и жарко спорят, кто лучше: iOS или Android &#128521;\n\nСлушайте Стича в его подкасте SAPOD — vk.com/sapod &#128072;')
 			
-			elif payload == '{"command":"back"}' and id != 2000000002:
+			elif state == '{"command":"back"}' and id != 2000000002:
 				msg(id, 'Возвращаю Вас в главное меню. Напоминаю назначение кнопок: \n\n#idea — идеи и предложения \n#partnership — партнёрство, сотрудничество, спонсорство \n#support — администрация, помощь и вопросы \n#buy — магазин услуг и покупки \n#news — последние новости из сферы IT', keyboards.menu)
 
 
 		if id == 2000000002:
-			if payload == '{"command":"request"}':
+			if state_chat == '{"command":"request"}':
 				msg(id, 'Решили запросить у кого-то деньги? У кого? Отправьте id пользователя.', keyboards.back)
 				state_chat = 'wait request_id'
 
@@ -139,7 +143,7 @@ for event in longpoll.listen():
 					print(err)
 					print(traceback.format_exc())
 				
-			elif payload == '{"command":"restart"}':
+			elif state_chat == '{"command":"restart"}':
 				msg(id, 'Решили перезапустить меня? У кого? Отправьте id пользователя.', keyboards.back)
 				state_chat = 'wait restart_id'
 
@@ -149,7 +153,7 @@ for event in longpoll.listen():
 				msg(restart_id, 'Привет, это снова я, Ваш любимый PTBot! &#128075; Добро пожаловать в старое доброе меню! &#128526;', keyboards.menu)
 				msg(id, 'Баг-перезапуск у [id{0}|{1} {2}] прошёл успешно!'.format(restart_id, name(restart_id, 'gen')['first_name'], name(restart_id, 'gen')['last_name']), keyboards.chat)
 
-			elif payload == '{"command":"back"}':
+			elif state_chat == '{"command":"back"}':
 				msg(id, 'Возвращаю вас в главное меню.', keyboards.chat)
 
 
