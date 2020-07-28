c5ff61d4d6ad3fe05e6a0d446aedc54b41e93a68
PaveTranquil <pasha.2ovchinnikov@gmail.com>
2020-07-28 03:56:31 +0300

    Полный краш-фикс парсера новостей
    - Учтены все случаи работы news.yandex.ru так, что бот в любом случае парсит информацию с сайта и не падает.
    - Обработчик ошибок переработан после acef325.

--- news.py
+++ news.py
@@ -1,69 +1,76 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import time
 
-headers_gadgets, links_gadgets, descs_gadgets, originals_gadgets = [], [], [], []
-headers_internet, links_internet, descs_internet, originals_internet = [], [], [], []
-headers_games, links_games, descs_games, originals_games = [], [], [], []
+headers_gadgets, descs_gadgets, originals_gadgets = [], [], []
+headers_internet, descs_internet, originals_internet = [], [], []
+headers_games, descs_games, originals_games = [], [], []
 
 
 def refresh_gadgets():
-	global headers_gadgets, links_gadgets, descs_gadgets, originals_gadgets
-	headers_gadgets, links_gadgets, descs_gadgets, originals_gadgets = [], [], [], []
-	site = requests.get('https://news.yandex.ru/gadgets.html')
-	soup = bs(site.text, 'html.parser')
-	code_headers = soup.find(class_='page-content').find_all(class_='link link_theme_black i-bem')
+    try:
+        global headers_gadgets, descs_gadgets, originals_gadgets
+        headers_gadgets, descs_gadgets, originals_gadgets = [], [], []
+        soup = bs(requests.get('https://yandex.ru/news/rubric/gadgets').text, 'html.parser')
+        while not soup.find(class_='page-content'):
+            soup = bs(requests.get('https://yandex.ru/news/rubric/gadgets').text, 'html.parser')
+        code_headers = soup.find_all(class_='link link_theme_black i-bem')
 
-	for i in range(0, 8):
-		links_gadgets.append('https://yandex.ru' + code_headers[i].get('href'))
-		headers_gadgets.append(code_headers[i].contents[0])
-
-	for i in range(0, 8):
-		article_site = requests.get(links_gadgets[i])
-		article_soup = bs(article_site.text, 'html.parser')
-		descs_gadgets.append(article_soup.find(class_='doc__text').contents[0])
-		originals_gadgets.append(article_soup.find(class_='doc__content').find('a').get('href'))
+        for i in range(0, 8):
+            headers_gadgets.append(code_headers[i].contents[0])
+            article_soup = bs(requests.get('https://yandex.ru' + code_headers[i].get('href')).text, 'html.parser')
+            while not article_soup.find(class_='doc__text') or not article_soup.find(class_='doc__content'):
+                article_soup = bs(requests.get('https://yandex.ru' + code_headers[i].get('href')).text, 'html.parser')
+            descs_gadgets.append(article_soup.find(class_='doc__text').contents[0])
+            originals_gadgets.append(article_soup.find(class_='doc__content').find('a').get('href'))
+    except Exception:
+        print('Новости гаджетов не обновлены!')
 
 
 def refresh_internet():
-	global headers_internet, links_internet, descs_internet, originals_internet
-	headers_internet, links_internet, descs_internet, originals_internet = [], [], [], []
-	site = requests.get('https://news.yandex.ru/internet.html')
-	soup = bs(site.text, 'html.parser')
-	code_headers = soup.find(class_='page-content').find_all(class_='link link_theme_black i-bem')
+    try:
+        global headers_internet, descs_internet, originals_internet
+        headers_internet, descs_internet, originals_internet = [], [], []
+        soup = bs(requests.get('https://yandex.ru/news/rubric/internet').text, 'html.parser')
+        while not soup.find(class_='page-content'):
+            soup = bs(requests.get('https://yandex.ru/news/rubric/internet').text, 'html.parser')
+        code_headers = soup.find_all(class_='link link_theme_black i-bem')
 
-	for i in range(0, 8):
-		links_internet.append('https://yandex.ru' + code_headers[i].get('href'))
-		headers_internet.append(code_headers[i].contents[0])
-
-	for i in range(0, 8):
-		article_site = requests.get(links_internet[i])
-		article_soup = bs(article_site.text, 'html.parser')
-		descs_internet.append(article_soup.find(class_='doc__text').contents[0])
-		originals_internet.append(article_soup.find(class_='doc__content').find('a').get('href'))
+        for i in range(0, 8):
+            headers_internet.append(code_headers[i].contents[0])
+            article_soup = bs(requests.get('https://yandex.ru' + code_headers[i].get('href')).text, 'html.parser')
+            while not article_soup.find(class_='doc__text') or not article_soup.find(class_='doc__content'):
+                article_soup = bs(requests.get('https://yandex.ru' + code_headers[i].get('href')).text, 'html.parser')
+            descs_internet.append(article_soup.find(class_='doc__text').contents[0])
+            originals_internet.append(article_soup.find(class_='doc__content').find('a').get('href'))
+    except Exception:
+        print('Новости интернета не обновлены!')
 
 
 def refresh_games():
-	global headers_games, links_games, descs_games, originals_games
-	headers_games, links_games, descs_games, originals_games = [], [], [], []
-	site = requests.get('https://yandex.ru/news/rubric/games')
-	soup = bs(site.text, 'html.parser')
-	code_headers = soup.find_all(class_='link link_theme_black i-bem')
+    try:
+        global headers_games, descs_games, originals_games
+        headers_games, descs_games, originals_games = [], [], []
+        soup = bs(requests.get('https://yandex.ru/news/rubric/games').text, 'html.parser')
+        while not soup.find(class_='page-content'):
+            soup = bs(requests.get('https://yandex.ru/news/rubric/games').text, 'html.parser')
+        code_headers = soup.find_all(class_='link link_theme_black i-bem')
+        if not code_headers:
+            code_headers = soup.find_all(class_='link link_theme_black i-bem link_js_inited')
 
-	for i in range(0, 8):
-		links_games.append('https://yandex.ru' + code_headers[i].get('href'))
-		headers_games.append(code_headers[i].contents[0])
-
-	for i in range(0, 8):
-		article_site = requests.get(links_games[i])
-		article_soup = bs(article_site.text, 'html.parser')
-		try:
-			descs_games.append(article_soup.find(class_='doc__content').find('div').contents[0])
-		except:
-			descs_games.append(article_soup.find(class_='news-story__content').find('span').contents[0])
-		try:
-			originals_games.append(article_soup.find(class_='doc__content').find('a').get('href'))
-		except:
-			originals_games.append(article_soup.find(class_='news-story__content').find('a').get('href'))
+        for i in range(0, 8):
+            headers_games.append(code_headers[i].contents[0])
+            article_soup = bs(requests.get('https://yandex.ru' + code_headers[i].get('href')).text, 'html.parser')
+            try:
+                descs_games.append(article_soup.find(class_='doc__content').find('div').contents[0])
+            except:
+                descs_games.append(article_soup.find(class_='news-story__content').find('span').contents[0])
+            try:
+                originals_games.append(article_soup.find(class_='doc__content').find('a').get('href'))
+            except:
+                originals_games.append(article_soup.find(class_='news-story__content').find('a').get('href'))
+    except Exception:
+        print('Новости игр не обновлены!')
 
 
 refresh_games()
